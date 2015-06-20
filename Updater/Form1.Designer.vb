1062148de12004af93f1bafaf8309c0e1e1f131f
Federico Rapetti <fuji1097@gmail.com>
2015-06-20 15:08:17 +0200

    Initial release

---
+++ Updater/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblText1 = New System.Windows.Forms.Label()
+        Me.lblCurrentVer = New System.Windows.Forms.Label()
+        Me.lblText2 = New System.Windows.Forms.Label()
+        Me.lblLatestVersion = New System.Windows.Forms.Label()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'lblText1
+        '
+        Me.lblText1.AutoSize = True
+        Me.lblText1.Location = New System.Drawing.Point(13, 13)
+        Me.lblText1.Name = "lblText1"
+        Me.lblText1.Size = New System.Drawing.Size(86, 13)
+        Me.lblText1.TabIndex = 0
+        Me.lblText1.Text = "Versione attuale:"
+        '
+        'lblCurrentVer
+        '
+        Me.lblCurrentVer.AutoSize = True
+        Me.lblCurrentVer.BackColor = System.Drawing.Color.LemonChiffon
+        Me.lblCurrentVer.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCurrentVer.Location = New System.Drawing.Point(102, 13)
+        Me.lblCurrentVer.Name = "lblCurrentVer"
+        Me.lblCurrentVer.Size = New System.Drawing.Size(25, 13)
+        Me.lblCurrentVer.TabIndex = 1
+        Me.lblCurrentVer.Text = "---"
+        '
+        'lblText2
+        '
+        Me.lblText2.AutoSize = True
+        Me.lblText2.Location = New System.Drawing.Point(196, 13)
+        Me.lblText2.Name = "lblText2"
+        Me.lblText2.Size = New System.Drawing.Size(82, 13)
+        Me.lblText2.TabIndex = 2
+        Me.lblText2.Text = "Ultima versione:"
+        '
+        'lblLatestVersion
+        '
+        Me.lblLatestVersion.AutoSize = True
+        Me.lblLatestVersion.BackColor = System.Drawing.Color.LemonChiffon
+        Me.lblLatestVersion.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLatestVersion.Location = New System.Drawing.Point(280, 12)
+        Me.lblLatestVersion.Name = "lblLatestVersion"
+        Me.lblLatestVersion.Size = New System.Drawing.Size(25, 13)
+        Me.lblLatestVersion.TabIndex = 3
+        Me.lblLatestVersion.Text = "---"
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Location = New System.Drawing.Point(3, 117)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(38, 13)
+        Me.lblInfo.TabIndex = 4
+        Me.lblInfo.Text = "Pronto"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(16, 46)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(179, 17)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Ignora gli aggiornamenti in futuro"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(283, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Ignora"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 85)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(341, 23)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(366, 133)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.lblLatestVersion)
+        Me.Controls.Add(Me.lblText2)
+        Me.Controls.Add(Me.lblCurrentVer)
+        Me.Controls.Add(Me.lblText1)
+        Me.Name = "Form1"
+        Me.Text = "CPs updater"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblText1 As Label
+    Friend WithEvents lblCurrentVer As Label
+    Friend WithEvents lblText2 As Label
+    Friend WithEvents lblLatestVersion As Label
+    Friend WithEvents lblInfo As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ Updater/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System
+Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports System.Collections.Generic
+
+Public Class Form1
+
+    Const appName = "{0AGHJ15F-G95D-06F4-KM90-G9XZI5DON37D}_is1"
+    Const url = "https://api.github.com/repos/Bl00d-Kirito/HSK-Cremisi_Portals_Demo/releases/latest"
+    Dim currentVer As String
+    Dim latestVer As String
+    Dim data As String
+    Dim jsonObject As JObject
+    Dim items As List(Of JToken)
+    Dim jsonReader As StreamReader
+    Dim haveNew As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        currentVer = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\" & appName, "DisplayVersion", Nothing)
+        If currentVer Is Nothing Then
+            ' TODO
+            currentVer = "---"
+        End If
+        Console.WriteLine("Current version: " & currentVer)
+        lblCurrentVer.Text = currentVer
+
+        latestVer = "---"
+        Try
+            Dim request As HttpWebRequest = HttpWebRequest.Create(url)
+            request.Proxy = Nothing
+            request.UserAgent = "Test"
+
+            Dim response As HttpWebResponse = request.GetResponse
+            Dim responseStream As System.IO.Stream = response.GetResponseStream
+
+            Dim streamReader As New System.IO.StreamReader(responseStream)
+            data = streamReader.ReadToEnd
+            streamReader.Close()
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+            MsgBox("Impossibile controllare gli aggiornamenti" & vbCrLf & ex.Message, MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Aggiornamento fallito")
+        End Try
+
+        If Not data Is Nothing Then
+            Dim ser As JObject = JObject.Parse(data)
+            Dim jData As List(Of JToken) = ser.Children().ToList
+
+            For Each item As JProperty In jData
+                item.CreateReader()
+                If item.Name = "tag_name" Then
+                    latestVer = item.Value.Value(Of String)
+                    Console.WriteLine("Latest version: " & latestVer)
+                    lblLatestVersion.Text = latestVer
+                End If
+            Next
+        End If
+        If Not currentVer Is Nothing And Not latestVer Is Nothing Then
+            haveNew = checkVersion(currentVer, latestVer)
+            If haveNew = 1 Then
+                Console.WriteLine("New version avaiable: " & latestVer & "(> " & currentVer & ")")
+            ElseIf haveNew = -1 Then
+                Console.WriteLine("Not-avaiable version: " & latestVer & "(< " & currentVer & ")")
+            ElseIf haveNew = 0 Then
+                Console.WriteLine("Updated version: " & latestVer & "(= " & currentVer & ")")
+            End If
+        End If
+    End Sub
+
+    Private Function checkVersion(prev As String, last As String)
+        Dim prevArray As List(Of String) = New List(Of String)
+        Dim lastArray As List(Of String) = New List(Of String)
+        Dim prevValue As Integer
+        Dim lastValue As Integer
+        Dim haveNew As Integer = 0
+        Dim i As Integer = 0
+        prevArray.AddRange(Split(prev, "."))
+        lastArray.AddRange(Split(last, "."))
+        prevArray(0) = prevArray(0).Trim("v")
+        lastArray(0) = lastArray(0).Trim("v")
+
+        Do While haveNew = 0
+            Try
+                prevValue = CInt(prevArray(i))
+            Catch ex As Exception
+                prevValue = 0
+            End Try
+            Try
+                lastValue = CInt(lastArray(i))
+            Catch ex As Exception
+                lastValue = 0
+            End Try
+            If prevValue > lastValue Then
+                haveNew = -1
+            ElseIf prevValue < lastValue Then
+                haveNew = 1
+            End If
+            If i > prevArray.Count - 1 And i > lastArray.Count - 1 Then
+                Exit Do
+            End If
+            i = i + 1
+        Loop
+        Return haveNew
+    End Function
+End Class
